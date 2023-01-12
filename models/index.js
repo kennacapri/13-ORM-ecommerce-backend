5754c10b1495b08568e99ab8efcb93371497ba94
Kenna Fackrell <kennac28@gmail.com>
2023-01-12 14:58:39 -0700

    added associations to index.js file

--- models/index.js
+++ models/index.js
@@ -4,12 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+});
+ // Products belongsTo Category 
+Category.hasMany(Product, {
+
+});
 
 // Categories have many Products
-
+Product.belongToMany(Tags, {
+through: {
+  model: ProductTag,
+  unique: false
+},
+as: 'product_tags',
+});
 // Products belongToMany Tags (through ProductTag)
+Tag.belongToMany(Product, {
 
+});
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
